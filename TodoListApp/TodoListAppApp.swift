a6b37c5734d73c983808b1d8c45dce99d6797b15
VinnyBui <vinh.bui0101@gmail.com>
2024-03-10 21:54:29 -0700

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Vinh Bui on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
